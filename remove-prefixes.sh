3e09425af58a9c7a6b6391e2ffc8ab395b8b243d
Jim Raden <james.raden@gmail.com>
2022-11-29 08:14:28 -0500

    Fix typo in sed expressions

--- remove-prefixes.sh
+++ remove-prefixes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-    sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  /g"
+    sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  //g"
 elif [ -z "$2" ]; then
     export FILENAME=`mktemp`
     sed "s/External Withdrawal - //g" <"$1" >$FILENAME && mv $FILENAME "$1"
@@ -9,5 +9,5 @@ elif [ -z "$2" ]; then
     sed "s/POS Signature Purchase -  //g" <"$1" >$FILENAME && mv $FILENAME "$1"
     sed "s/Point Of Sale Deposit -  //g" <"$1" >$FILENAME && mv $FILENAME "$1"
 else
-    cat "$1" | sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  /g" >"$2"
+    cat "$1" | sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  //g" >"$2"
 fi
