8a85db5200a614bfaae1290525675d0393d0e2da
Jim Raden <james.raden@gmail.com>
2024-03-28 16:44:57 -0400

    Remove "POS Pinned Purchase"

--- remove-prefixes.sh
+++ remove-prefixes.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-    sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  //g"
+    sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  //g" | sed "s/POS Pinned Purchase -  //g"
 elif [ -z "$2" ]; then
     export FILENAME=`mktemp`
     sed "s/External Withdrawal - //g" <"$1" >$FILENAME && mv $FILENAME "$1"
     sed "s/External Deposit - //g" <"$1" >$FILENAME && mv $FILENAME "$1"
     sed "s/POS Signature Purchase -  //g" <"$1" >$FILENAME && mv $FILENAME "$1"
     sed "s/Point Of Sale Deposit -  //g" <"$1" >$FILENAME && mv $FILENAME "$1"
+    sed "s/POS Pinned Purchase -  //g" <"$1" >$FILENAME && mv $FILENAME "$1"
 else
-    cat "$1" | sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  //g" >"$2"
+    cat "$1" | sed "s/External Withdrawal - //g" | sed "s/External Deposit - //g" | sed "s/POS Signature Purchase -  //g" | sed "s/Point Of Sale Deposit -  //g" | sed "s/POS Pinned Purchase -  //g" >"$2"
 fi
